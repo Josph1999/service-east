54ab0259b797203b8037242ff937312e64202a09
iosebi1999 <iosebi@hlwgroup.dev>
2024-08-07 12:34:17 +0400

    Added contact form

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import React from 'react';
-import { Box, Button, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Button, FormHelperText, TextField, Typography, Snackbar, Alert } from '@mui/material';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import { useLanguage } from '@/contexts/language-context';
 
@@ -9,13 +11,58 @@ import GoogleMaps from '../google-maps/google-maps';
 import DirectionIcon from '../icons/direction';
 import MailIcon from '../icons/mail';
 import PhoneIcon from '../icons/phone';
-import Subscribe from '../subscribe/subscribe';
 
 export default function Contact(): React.JSX.Element {
   const { renderLanguage } = useLanguage();
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const formik = useFormik({
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .required(renderLanguage('სახელი სავალდებულოა', 'Name is required'))
+        .max(20, renderLanguage('მაქს 20 ', 'Max 20')),
+      last_name: Yup.string()
+        .required(renderLanguage('გვარი სავალდებულოა', 'Lastname is required'))
+        .max(20, renderLanguage('მაქს 20 ', 'Max 20')),
+      email: Yup.string()
+        .email()
+        .required(renderLanguage('მეილი სავალდებულოა', 'Mail is required'))
+        .max(130, renderLanguage('მაქს 130 ', 'Max 130')),
+      subject: Yup.string()
+        .required(renderLanguage('სათაური სავალდებულოა', 'Subject is required'))
+        .max(50, renderLanguage('მაქს 50 ', 'Max 50')),
+      message: Yup.string()
+        .required(renderLanguage('აღწერა სავალდებულოა', 'Message is required'))
+        .max(250, renderLanguage('მაქს 250 ', 'Max 250')),
+    }),
+    initialValues: {
+      name: '',
+      last_name: '',
+      email: '',
+      subject: '',
+      message: '',
+      phone_number: null,
+    },
+    onSubmit: (values, currFormik) => {
+      console.log('Values:', values);
+      setOpen(true);
+      currFormik.resetForm();
+    },
+  });
 
   return (
-    <>
+    <Box
+      sx={{
+        backgroundImage: `url(/assets/Rectangle.png)`, // replace with your SVG file path
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'contain',
+        backgroundPosition: 'right',
+      }}
+    >
       <Box
         sx={{
           padding: '128px',
@@ -54,7 +101,7 @@ export default function Contact(): React.JSX.Element {
             }}
           >
             <a
-              href="tel:+995595330057"
+              href="tel:+380443443901"
               style={{
                 color: '#4338CA',
                 textDecoration: 'none',
@@ -79,7 +126,7 @@ export default function Contact(): React.JSX.Element {
                   },
                 }}
               >
-                <PhoneIcon /> (+995) 595 33 00 57
+                <PhoneIcon /> +380 (44) 344 39 01
               </Box>
             </a>
           </Button>
@@ -92,40 +139,34 @@ export default function Contact(): React.JSX.Element {
               color: '#232C65',
             }}
           >
-            <a
-              style={{
+            <Box
+              sx={{
                 color: '#4338CA',
                 textDecoration: 'none',
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
                 gap: '10px',
-              }}
-              href="/"
-            >
-              <Box
-                sx={{
-                  color: '#4338CA',
-                  textDecoration: 'none',
+                '@media (max-width: 800px)': {
                   display: 'flex',
                   justifyContent: 'center',
-                  alignItems: 'center',
-                  gap: '10px',
-                  '@media (max-width: 800px)': {
-                    display: 'flex',
-                    justifyContent: 'center',
-                    flexDirection: 'column',
-                  },
-                }}
-              >
-                {' '}
-                <DirectionIcon />{' '}
-                {renderLanguage(
-                  'მერაბ კოსტავას ქუჩა, 75ბ, 0112, თბილისი, საქართველო',
-                  '75B, Merab Kostava Street, 0112, Tbilisi, Georgia'
-                )}
-              </Box>
-            </a>
+                  flexDirection: 'column',
+                },
+              }}
+            >
+              {' '}
+              <DirectionIcon />{' '}
+              {renderLanguage(
+                `SERVICE EAST LLC
+"Horizon Office Tower"
+42-44 Shovkovychna Str, office 2-A
+01601 Kyiv Ukraine`,
+                `SERVICE EAST LLC
+"Horizon Office Tower"
+42-44 Shovkovychna Str, office 2-A
+01601 Kyiv Ukraine`
+              )}
+            </Box>
           </Button>
           <Button
             fullWidth
@@ -137,7 +178,7 @@ export default function Contact(): React.JSX.Element {
             }}
           >
             <a
-              href="mailto:info@act.org.ge"
+              href="mailto:info@service-east.com"
               style={{
                 color: '#4338CA',
                 textDecoration: 'none',
@@ -162,14 +203,110 @@ export default function Contact(): React.JSX.Element {
                   },
                 }}
               >
-                <MailIcon /> Info@act.org.ge
+                <MailIcon /> info@service-east.com
               </Box>
             </a>
           </Button>
         </Box>
         <GoogleMaps />
+        <form noValidate onSubmit={formik.handleSubmit}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: '40px', marginTop: '80px' }}>
+            <Typography sx={{ fontSize: '26px', fontWeight: 700, fontFeatureSettings: "'case' on" }}>
+              {renderLanguage('კონტაქტის ფორმა', 'Contact Form')}
+            </Typography>
+
+            <Box
+              sx={{
+                display: 'flex',
+                gap: '150px',
+                '@media (max-width: 700px)': {
+                  flexDirection: 'column',
+                  gap: '40px',
+                },
+              }}
+            >
+              <TextField
+                error={Boolean(formik.touched.name && formik.errors.name)}
+                helperText={formik.touched.name ? formik.errors.name : null}
+                label={renderLanguage('სახელი', 'Name')}
+                name="name"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                required
+                fullWidth
+                variant="standard"
+              />
+              <TextField
+                error={Boolean(formik.touched.last_name && formik.errors.last_name)}
+                helperText={formik.touched.last_name ? formik.errors.last_name : null}
+                label={renderLanguage('გვარი', 'Last Name')}
+                name="last_name"
+                onBlur={formik.handleBlur}
+                required
+                onChange={formik.handleChange}
+                value={formik.values.last_name}
+                fullWidth
+                variant="standard"
+              />
+            </Box>
+            <TextField
+              error={Boolean(formik.touched.email && formik.errors.email)}
+              helperText={formik.touched.email ? formik.errors.email : null}
+              label={renderLanguage('მეილი', 'Email')}
+              name="email"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              required
+              value={formik.values.email}
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              error={Boolean(formik.touched.subject && formik.errors.subject)}
+              helperText={formik.touched.subject ? formik.errors.subject : null}
+              label={renderLanguage('სათაური', 'Subject')}
+              name="subject"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              required
+              value={formik.values.subject}
+              fullWidth
+              variant="standard"
+            />
+            <textarea
+              name="message"
+              value={formik.values.message}
+              onChange={formik.handleChange}
+              required
+              onBlur={formik.handleBlur}
+              placeholder={renderLanguage('აღწერა მაქსიმუმ 250 სიმბოლო *', 'Message Max 250 symbols *')}
+              style={{
+                width: '100%',
+                border: 'none',
+                borderBottom: '1px solid #000',
+                resize: 'none',
+                padding: '8px 0',
+                fontFamily: 'UpperCaseGeo',
+                fontSize: '16px',
+                backgroundColor: 'transparent',
+              }}
+              rows={5}
+            />
+            <FormHelperText sx={{ color: 'red' }}>
+              {formik.touched.message ? formik.errors.message : null}
+            </FormHelperText>
+            <Button variant="outlined" type="submit">
+              {renderLanguage('გაგზავნა', 'Send')}
+            </Button>
+          </Box>
+        </form>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}   anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+          <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }} >
+            {renderLanguage('ფორმა წარმატებით გაიგზავნა!', 'Form submitted successfully!')}
+          </Alert>
+        </Snackbar>
       </Box>
-      <Subscribe />
-    </>
+    </Box>
   );
 }
--- src/components/google-maps/google-maps.tsx
+++ src/components/google-maps/google-maps.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function GoogleMaps(): React.JSX.Element {
   const iframeCode = `
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d372.2434769443441!2d44.7791248!3d41.7216449!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x404472d73c136823%3A0x5d9c9aef5cebd45e!2zNzViIOGDm-GDlOGDoOGDkOGDkSDhg5nhg53hg6Hhg6Lhg5Dhg5Xhg5Dhg6Eg4YOl4YOj4YOp4YOQLCDhg5fhg5Hhg5jhg5rhg5jhg6Hhg5ggMDE3OQ!5e0!3m2!1ska!2sge!4v1708857546637!5m2!1ska!2sge" width="100%" height="639px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1270.5195059762714!2d30.526722!3d50.440374!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cf0002d2aed7%3A0x41c584d0b7cfe4eb!2sHorizon%20Office%20Tower!5e0!3m2!1sru!2sua!4v1723016505774!5m2!1sru!2sua" width="100%" height="639px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     `;
 
   return <div dangerouslySetInnerHTML={{ __html: iframeCode }} />;
